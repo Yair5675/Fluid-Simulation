7df96e5e697829bad876526ba38b1b4c233cf2d1
Yair5675 <yairziv5675@gmail.com>
2025-11-06 19:49:59 +0200

    chore: Create ipc module.
    Will define shared data types + protocol traits which will abstract the communication between the frontend module and the backend module

---
+++ src/ipc/mod.rs
@@ -0,0 +1,3 @@
+//! Responsible for containing data types, structs and traits which define and abstract the
+//! communication between the frontend module and the backend module.<br>
+//! This isn't technically an IPC since both run on the same process, but represents the same idea.
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod frontend;
 mod backend;
+pub mod ipc;
 
 fn main() {
     println!("Hello, world!");
