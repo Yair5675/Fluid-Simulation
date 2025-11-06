0e484e0e57f94aeae89f8b6740df300e38423206
Yair5675 <yairziv5675@gmail.com>
2025-11-06 20:51:35 +0200

    chore: Define SimulationGrid trait.
    The trait abstracts the data structure which is passed from the backend to the frontend. It allows the data to take any shape, and also have any meaning (for example it can be a grid of pressure values, a grid of velocity magnitudes etc...)

--- src/ipc/mod.rs
+++ src/ipc/mod.rs
@@ -1,3 +1,30 @@
 //! Responsible for containing data types, structs and traits which define and abstract the
-//! communication between the frontend module and the backend module.<br>
+//! communication between the frontend module and the backend module.
+//!
 //! This isn't technically an IPC since both run on the same process, but represents the same idea.
+
+/// Represents a grid of computed values which should be passed from the backend to the frontend.
+///
+/// The implementation is not defined as a struct to allow flexible refactoring of the values'
+/// container if such need is encountered (for performance reasons for example).
+///
+/// Note that this trait is meant to be a read-only protocol, aimed to be sent from backend to
+/// frontend.
+pub trait SimulationGrid<'a, T> {
+    /// Returns the dimensions of the grid (its length in the horizontal and vertical dimensions).
+    ///
+    /// # Return value
+    /// A tuple whose first element is the grid's *horizontal* length, and whose second is the
+    /// grid's *vertical* length.
+    fn get_grid_dimensions(&self) -> (usize, usize);
+
+    /// Retrieves a reference to a value at a specific location in the grid.
+    ///
+    /// # Arguments
+    /// * `x` - The horizontal coordinate of the value in the grid.
+    /// * `y` - The vertical coordinate of the value in the grid.
+    ///
+    /// # Return value
+    /// An immutable reference to the grid value at coordinates (`x`, `y`).
+    fn get_value_at(&self, x: usize, y: usize) -> &'a T;
+}
