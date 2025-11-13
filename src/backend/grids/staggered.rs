3140e21e877ae7029c79e450d81e884a68454bec
Yair5675 <yairziv5675@gmail.com>
2025-11-13 22:27:58 +0200

    refactor: Turn StaggeredValocityField into a tuple struct
    Since StaggeredVelocityField was already just a specialized wrapper around a one-dimensional grid, and it was the only field, one could see why this change was made

--- src/backend/grids/staggered.rs
+++ src/backend/grids/staggered.rs
@@ -5,10 +5,7 @@ use vector2d::Vector2D;
 use crate::backend::grids::OneDimensionalGrid;
 
 /// A special type of grid which holds velocity values at the center of each cell's edges.
-pub struct StaggeredVelocityField {
-    /// The actual data-structure storing the staggered velocities
-    raw_grid: OneDimensionalGrid<Vector2D<f64>>, // Implementation detail - OneDimensionalGrid used (any Grid will do).
-}
+pub struct StaggeredVelocityField(OneDimensionalGrid<Vector2D<f64>>);
 
 impl StaggeredVelocityField {
     /// Creates a new `StaggeredVelocityField` where all of the stored velocity vectors are set to
@@ -23,6 +20,6 @@ impl StaggeredVelocityField {
     pub fn new(width: usize, height: usize) -> Self {
         // Since we have 4 values per cell instead of 2, we need to add 1 row and 1 column.
         let raw_grid = OneDimensionalGrid::new(width + 1, height + 1);
-        Self { raw_grid }
+        Self(raw_grid)
     }
 }
