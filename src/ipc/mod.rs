327f59a4a22f0f4ebfdff97ba8d5944878bd3e2c
Yair5675 <yairziv5675@gmail.com>
2025-11-15 19:33:57 +0200

    refactor: Replace SimulationGrid trait with concrete Grid struct

--- src/ipc/mod.rs
+++ src/ipc/mod.rs
@@ -3,40 +3,11 @@
 //!
 //! This isn't technically an IPC since both run on the same process, but represents the same idea.
 
-use std::sync::Arc;
-
-/// Represents a grid of computed values which should be passed from the backend to the frontend.
-///
-/// The implementation is not defined as a struct to allow flexible refactoring of the values'
-/// container if such need is encountered (for performance reasons for example).
-///
-/// Note that this trait is meant to be a read-only protocol, aimed to be sent from backend to
-/// frontend.
-pub trait SimulationGrid {
-    /// The specific kind of data the simulation grid holds.
-    type Data;
-
-    /// Returns the dimensions of the grid (its length in the horizontal and vertical dimensions).
-    ///
-    /// # Return value
-    /// A tuple whose first element is the grid's *horizontal* length, and whose second is the
-    /// grid's *vertical* length.
-    fn get_grid_dimensions(&self) -> (usize, usize);
-
-    /// Retrieves a reference to a value at a specific location in the grid.
-    ///
-    /// # Arguments
-    /// * `x` - The horizontal coordinate of the value in the grid.
-    /// * `y` - The vertical coordinate of the value in the grid.
-    ///
-    /// # Return value
-    /// An immutable reference to the grid value at coordinates (`x`, `y`).
-    fn get_value_at(&self, x: usize, y: usize) -> &Self::Data;
-}
+use crate::backend::grid::Grid;
 
 /// Represents the different types of simulation data which can be sent from the backend to the
 /// frontend.
 pub enum SimulationData {
     /// A grid of pressure values.
-    Pressure(Arc<dyn SimulationGrid<Data = f64>>),
+    Pressure(Grid<f64>),
 }
