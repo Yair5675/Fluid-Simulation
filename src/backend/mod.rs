bfd58111d4d072daa1edc9e1d7a26a37c800f82b
Yair5675 <yairziv5675@gmail.com>
2025-12-20 15:56:52 +0200

    feat: Initialize the engine in main backend struct using BackendConfiguration

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,9 +1,12 @@
 //! Handles all logic of the fluid simulation including physics, simulation configuration, etc...
 
-use std::sync::{Arc, RwLock, atomic::AtomicBool};
-
-use self::configuration::BackendConfiguration;
+use self::{
+    configuration::BackendConfiguration,
+    engine::{EngineOutput, SimulationEngine},
+    pool::Pool,
+};
 use crate::ipc::SimulationData;
+use std::sync::{Arc, RwLock, atomic::AtomicBool};
 
 pub mod configuration;
 pub mod engine;
@@ -18,6 +21,8 @@ pub struct FluidSimulationBackend {
     is_running: AtomicBool,
     config: BackendConfiguration,
     currently_rendering_data: RwLock<Arc<SimulationData>>,
+    engine: SimulationEngine,
+    engine_output_pool: Arc<Pool<EngineOutput>>,
 }
 
 impl FluidSimulationBackend {
@@ -30,10 +35,32 @@ impl FluidSimulationBackend {
     /// # Return Value:
     /// A new `FluidSimulationBackend`, configured as requested.
     pub fn new(configuration: BackendConfiguration) -> Self {
+        let pool = Arc::new(if let Some(pool_limit) = configuration.engine_pool_limit {
+            Pool::new_bounded(pool_limit.get())
+        } else {
+            Pool::new_unbounded()
+        });
+
         Self {
             is_running: AtomicBool::new(false),
             config: configuration,
             currently_rendering_data: RwLock::new(Arc::new(SimulationData::Loading)),
+            engine: Self::initialize_engine(&configuration, Arc::clone(&pool)),
+            engine_output_pool: pool,
         }
+    }
+
+    fn initialize_engine(
+        config: &BackendConfiguration,
+        engine_pool: Arc<Pool<EngineOutput>>,
+    ) -> SimulationEngine {
+        SimulationEngine::new(
+            config.particles_count.get(),
+            config.grid_width.get(),
+            config.grid_height.get(),
+            config.grid_spacing,
+            engine_pool,
+            config.engine,
+        )
     }
 }
